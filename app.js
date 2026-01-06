b76bfb8f372e05b603db9e3ec63e87b6a2cd2992
Sachin Jha <sachinjhagc@gmail.com>
2026-01-06 21:49:09 +0530

    uploaded from github file

---
+++ app.js
@@ -0,0 +1,341 @@
+const inp = document.querySelector("#img-inp");
+
+const canvas = document.querySelector("#img-box");
+
+let ctx = canvas.getContext("2d");
+
+let file = null;
+let image = null;
+
+
+// canvas.width = canvas.clientWidth;
+// canvas.height = canvas.clientHeight;
+
+const dpr = window.devicePixelRatio || 1;
+
+canvas.width = canvas.clientWidth * dpr;
+canvas.height = canvas.clientHeight * dpr;
+
+let newWidth = null; let x = null;
+let newHeight = null; let y = null;
+
+let imgGot = false;
+// ctx.scale(dpr, dpr);
+
+let placeholderText = document.querySelector(".placeholder");
+
+canvas.style.display = "none";
+
+// inp.addEventListener("click", () => {
+// location.reload();
+// })
+
+let firstUpload = false;
+
+inp.addEventListener("change", (event) => {
+
+    // if(firstUpload) {
+    //     location.reload();
+    // }
+    console.log("image input held");
+    canvas.style.display = "block";
+    placeholderText.style.display = "none";
+
+    file = event.target.files[0];
+    imgGot = true;
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+        image = img;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+        newWidth = img.width * scale;
+        newHeight = img.height * scale;
+
+        x = (canvas.width - newWidth) / 2;
+        y = (canvas.height - newHeight) / 2;
+
+        ctx.drawImage(img, x, y, newWidth, newHeight);
+    }
+
+});
+
+let brightValue = 100; let contrastValue = 100; let saturateValue = 100; let hueRotateValue = 0;
+let blurValue = 0; let grayscaleValue = 0; let sepiaValue = 0; let opacityValue = 100; let invertValue = 0;
+
+let brightText = document.getElementById("bright-text");
+let contrastText = document.getElementById("contrast-text");
+let saturateText = document.getElementById("saturate-text");
+let hueText = document.getElementById("hue-text");
+let blurText = document.getElementById("blur-text");
+let grayText = document.getElementById("gray-text");
+let sepiaText = document.getElementById("sepia-text");
+let opacityText = document.getElementById("opacity-text");
+let invertText = document.getElementById("invert-text");
+// Apply filter on slider movement
+
+
+let tic_color = document.querySelector(".tic-btn svg");
+
+let allInp = document.querySelectorAll(".right input");
+
+
+for (let btn of allInp) {
+    btn.addEventListener("input", (event) => {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        if (imgGot) {
+            tic_color.style.fill = "white";
+            isSliderMove = true;
+            if (event.target.name == "brightness") {
+                brightValue = event.target.value;
+                brightText.innerText = `200/${brightValue}`;
+            }
+            if (event.target.name == "contrast") {
+                contrastValue = event.target.value;
+                contrastText.innerText = `200/${contrastValue}`;
+            }
+            if (event.target.name == "saturation") {
+                saturateValue = event.target.value;
+                saturateText.innerText = `200/${saturateValue}`;
+            }
+            if (event.target.name == "hueRotation") {
+                hueRotateValue = event.target.value;
+                hueText.innerText = `360/${hueRotateValue}`;
+            }
+            if (event.target.name == "blur") {
+                blurValue = event.target.value;
+                blurText.innerText = `20/${blurValue}`;
+            }
+            if (event.target.name == "grayscale") {
+                grayscaleValue = event.target.value;
+                grayText.innerText = `100/${grayscaleValue}`;
+            }
+            if (event.target.name == "sepia") {
+                sepiaValue = event.target.value;
+                sepiaText.innerText = `100/${sepiaValue}`;
+            }
+            if (event.target.name == "opacity") {
+                opacityValue = event.target.value;
+                opacityText.innerText = `100/${opacityValue}`;
+            }
+            if (event.target.name == "invert") {
+                invertValue = event.target.value;
+                invertText.innerText = `100/${invertValue}`;
+            }
+
+            ctx.filter = `
+            brightness(${brightValue}%)
+            contrast(${contrastValue}%)
+            saturate(${saturateValue}%)
+            hue-rotate(${hueRotateValue}deg)
+            blur(${blurValue}px)
+            grayscale(${grayscaleValue}%)
+            sepia(${sepiaValue}%)
+            opacity(${opacityValue}%)
+            invert(${invertValue}%)
+            `;
+
+            ctx.drawImage(image, x, y, newWidth, newHeight);
+        }
+
+    });
+}
+
+
+
+let applyFilter = (name, value, unit) => {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.filter = `${name}(${value}${unit})`;
+
+    ctx.drawImage(image, x, y, newWidth, newHeight);
+}
+
+
+//  Remove filter
+
+
+let resetBtn = document.querySelector("#reset-btn");
+
+resetBtn.addEventListener("click", () => {
+    // console.log("reset");
+
+    for (let btn of allInp) {
+        if (imgGot) {
+            if (btn.name == "brightness") {
+                btn.value = 100;
+                applyFilter("brightness", 100, "%");
+                brightText.innerText = "200/100";
+            }
+            if (btn.name == "contrast") {
+                btn.value = 100;
+                applyFilter("contrast", 100, "%");
+                contrastText.innerText = "200/100";
+            }
+            if (btn.name == "saturation") {
+                btn.value = 100;
+                applyFilter("saturate", 100, "%");
+                saturateText.innerText = "200/100";
+            }
+            if (btn.name == "hueRotation") {
+                btn.value = 0;
+                applyFilter("hue-rotate", 0, "deg");
+                hueText.innerText = "360/0";
+            }
+            if (btn.name == "blur") {
+                btn.value = 0;
+                applyFilter("blur", 0, "px");
+                blurText.innerText = "20/0";
+            }
+            if (btn.name == "grayscale") {
+                btn.value = 0;
+                applyFilter("grayscale", 0, "%");
+                grayText.innerText = "100/0";
+            }
+            if (btn.name == "sepia") {
+                btn.value = 0;
+                applyFilter("sepia", 0, "%");
+                sepiaText.innerText = "100/0";
+            }
+            if (btn.name == "opacity") {
+                btn.value = 100;
+                applyFilter("opacity", 100, "%");
+                opacityText.innerText = "100/100";
+            }
+            if (btn.name == "invert") {
+                btn.value = 0;
+                applyFilter("invert", 0, "%");
+                opacityText.innerText = "100/0";
+            }
+        }
+    }
+});
+
+
+//  Download Image
+
+let downloadBtn = document.querySelector("#download-btn");
+
+downloadBtn.addEventListener("click", () => {
+    const link = document.createElement("a");
+    link.download = "edited-image.jpg";
+    link.href = canvas.toDataURL();
+    link.click();
+});
+
+
+// buttons for small screen
+
+// let brightBtn = document.querySelector("#bright");
+
+let isSliderMove = false;
+let tic_untic_container = document.querySelector(".tic-untic");
+
+let ticBtn = document.querySelector(".tic-btn");
+let unticBtn = document.querySelector(".untic-btn");
+
+ticBtn.addEventListener("click", () => {
+    if (isSliderMove) {
+        removeFilterInputs();
+        btnContainer.style.display = "flex";
+        tic_untic_container.style.display = "none";
+    }
+});
+
+unticBtn.addEventListener("click", () => {
+    removeFilterInputs();
+    btnContainer.style.display = "flex";
+    tic_untic_container.style.display = "none";
+});
+
+let inputContainer = document.querySelector(".filter-box");
+
+let filterContainer = document.querySelectorAll(".filter-sm-box");
+
+let filterArray = Array.from(filterContainer);
+
+let removeFilterInputs = () => {
+    for (let inp of filterArray) {
+        inp.style.display = "none";
+    }
+}
+
+removeFilterInputs();
+
+
+let btnContainer = document.querySelector(".btn-container");
+let allBtn = document.querySelectorAll(".btn-container .filter-btn");
+
+for (let btn of allBtn) {
+    btn.addEventListener("click", () => {
+
+        for (let BTN of allBtn) {
+            BTN.style.boxShadow = "0px 0px 0px slateblue";
+        }
+        btn.style.boxShadow = "0px 0px 16px dodgerblue";
+
+        let id = btn.getAttribute("id");
+
+        setTimeout(() => {
+            btnContainer.style.display = "none";
+            tic_untic_container.style.display = "flex";
+            tic_color.style.fill = "gray";
+        }, 250);
+
+        if (id == "brightness") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[0].style.display = "flex";
+
+            }, 250);
+
+        } else if (id == "contrast") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[1].style.display = "flex";
+            }, 250);
+
+        } else if (id == "saturation") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[2].style.display = "flex";
+            }, 250);
+        } else if (id == "hueRotation") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[3].style.display = "flex";
+            }, 250);
+        } else if (id == "blur") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[4].style.display = "flex";
+            }, 250);
+        } else if (id == "grayscale") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[5].style.display = "flex";
+            }, 250);
+        } else if (id == "sepia") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[6].style.display = "flex";
+            }, 250);
+        } else if (id == "opacity") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[7].style.display = "flex";
+            }, 250);
+        } else if (id == "invert") {
+            setTimeout(() => {
+                removeFilterInputs();
+                filterArray[8].style.display = "flex";
+            }, 250);
+        }
+    });
+}
